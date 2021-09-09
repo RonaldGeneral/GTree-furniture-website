885557edca721ebafb3aee894e7fc3d989fa00c4
LWMeng0822 <88777384+LWMeng0822@users.noreply.github.com>
2021-09-09 13:09:47 +0800

    Add files via upload

---
+++ GTree-furniture-website/Register/register.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var checkBox = document.getElementById("myCheck");
+    var text = document.getElementById("text");
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
+}
+
+function submit(){
+  window.location.href="../Login/login.html";
+  alert("Register Successfully");
+}
