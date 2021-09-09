15c075f831b5b7a5488773e07da9489fdec325fb
weixiang1120 <81514513+weixiang1120@users.noreply.github.com>
2021-09-09 12:09:40 +0800

    Add files via upload

--- GTree-furniture-website/shopping-cart/shopping-cart.js
+++ GTree-furniture-website/shopping-cart/shopping-cart.js
@@ -1,66 +1,155 @@
-const item = ["unselected","unselected","unselected"], itemQuantity = [0, 0, 0], itemPrice = [0, 0, 0];
-
-function selectAll(){
-    var allTick = true;
-    for (var i = 0; i < 3; i++) {
-        if ((!document.getElementById("item"+i).checked) && (item[i] != "deleted")){
-            allTick = false;
-            document.getElementById("item"+i).checked = true;
-            addItem(i, document.getElementById('quantity'+i).value, 9999);
-        }   
-    }
-    if (allTick == true){
-        for (var i = 0; i < 3; i++){
-            if (item[i] == "selected"){
-                document.getElementById("item"+i).checked = false;
-                addItem(i, document.getElementById('quantity'+i).value, 9999);
-            }
+fetch("product.json")
+    .then(response => response.json())
+    .then(data => {
+        var orderData = {};
+        orderData = data;
+        //retrieve data from local storage
+        Storage.prototype.setObj = function(key, obj) {
+            return this.setItem(key, JSON.stringify(obj))
         }
-        document.getElementById("selectAll").checked = false;
-    }
-}
+        Storage.prototype.getObj = function(key) {
+              return JSON.parse(this.getItem(key))
+        }
 
-function deleteItem(i,quantity, price){
-    quantity = parseInt(quantity);
-    price = parseInt(price);
-    document.getElementById("row"+i).style.display="none";
-    document.getElementById("selectAll").checked = false;
-    if (document.getElementById("item"+i).checked == true){
-        itemQuantity[i] = 0;
-        itemPrice[i] = 0;
-        document.getElementById("item"+i).checked = false;
-        addTotalItem();
-    }
-    item[i] = "deleted";
-}
-
-function addItem(i,quantity, price){
-    quantity = parseInt(quantity);
-    price = parseInt(price);
-    if (document.getElementById("item"+i).checked){
-        item[i] = "selected";
-        itemQuantity[i] = quantity;
-        itemPrice[i] = price * quantity;
-        addTotalItem();
-    }
-    if (!document.getElementById("item"+i).checked){
-        if (item[i] == "selected"){
+        localStorage.setObj("cartProduct", ["chair1", "chair2"]);
+        localStorage.setObj("cartQty", ["3", "5"]);
+        var product = localStorage.getObj("cartProduct");
+        var qty = localStorage.getObj("cartQty");
+        
+        const item = [], itemQuantity = [], itemPrice = [];
+        
+        //Display cart product according to items added to cart
+        for(var i = 0; i < product.length; i++){
+            var str = '<tr id="row$"><td><input type="checkbox" id="item$" name="chair"></td><td><img src="shopping-cart-assets/chair.jpg" alt="furniture $"></td><td><h2 class="name" id="name$">The Chair</h2></td><td><h2 class="unitPrice" id="unitPrice$">9999</h2></td><td><h2 class="quantity"><input type="number" id="quantity$" name="quantity" min="1" max="99" value="1" ></h2></td><td><h2 class="totalItemPrice" id="totalItemPrice$">9999</h2></td><td><button type="button" id=\"delete$\">&#10060;</button></td></tr>';
+            for (var j = 0; j < 17; j++)
+                str = str.replace("$", i);
+            document.getElementById("product").innerHTML += str;
+            document.getElementById("row"+i).style.display = "table-row";
+            document.getElementById("quantity"+i).value = qty[i];document.getElementById("quantity0").value = qty[0];
+            document.getElementById("name"+i).innerHTML = orderData[product[i]]["productName"];
+            document.getElementById("unitPrice"+i).innerHTML = orderData[product[i]]["unitPrice"];
+            document.getElementById("totalItemPrice"+i).innerHTML = orderData[product[i]]["unitPrice"] * qty[i];   
+            item[i] = "unselected";
             itemQuantity[i] = 0;
             itemPrice[i] = 0;
-            addTotalItem();
-            document.getElementById("selectAll").checked = false;
-            item[i] = "unselected";
+            
         }
-    }
-}
+        
+        function getItemIndex(obj){
+            var itemId = obj.id;
+            var index = itemId.split("item")[1];
+            return index;
+        }
 
-function addTotalItem(){
-    var totalPrice = 0, totalQuantity = 0;
-    for (var i = 0; i < 3; i++){
-        totalQuantity += itemQuantity[i];
-        totalPrice += itemPrice[i];
-    }
-    document.getElementById("totalItem").innerHTML = totalQuantity;
-    document.getElementById("totalPrice").innerHTML = totalPrice;
-}
+        function getQtyIndex(obj){
+            var qtyId = obj.id;
+            var index = qtyId.split("quantity")[1];
+            return index;
+        }
+
+        function getDltIndex(obj){
+            var dltId = obj.id;
+            var index = dltId.split("delete")[1];
+            return index;
+        }
+
+        //Add event listener to each item
+        for(var x = 0; x < product.length; x++){
+            document.getElementById("item"+x).addEventListener("click", function() {
+                var index = getItemIndex(this);
+                console.log(index);
+                addItem(index, document.getElementById("quantity"+index).value);});
+            document.getElementById("quantity"+x).addEventListener("change", function() {
+                var index = getQtyIndex(this);
+                console.log(index);
+                addItem(index, document.getElementById("quantity"+index).value);});
+            document.getElementById("delete"+x).addEventListener("click", function() {
+                var index = getDltIndex(this);
+                console.log(index);
+                deleteItem(index)});
+        }
+        document.getElementById("selectAll").addEventListener("click", function() {selectAll();});
+        document.getElementById("checkout").addEventListener("click", function() {checkout();});
+
+        function selectAll(){
+            var allTick = true;
+            for (var i = 0; i < product.length; i++) {
+                if ((!document.getElementById("item"+i).checked) && (item[i] != "deleted")){
+                    allTick = false;
+                    document.getElementById("item"+i).checked = true;
+                    addItem(i, document.getElementById('quantity'+i).value);
+                }   
+            }
+            if (allTick == true){
+                for (var i = 0; i < product.length; i++){
+                    if (item[i] == "selected"){
+                        document.getElementById("item"+i).checked = false;
+                        addItem(i, document.getElementById('quantity'+i).value);
+                    }
+                }
+                //document.getElementById("selectAll").checked = false;
+            }
+        }
+        
+        function deleteItem(i){
+            document.getElementById("row"+i).style.display="none";
+            document.getElementById("selectAll").checked = false;
+            if (document.getElementById("item"+i).checked == true){
+                itemQuantity[i] = 0;
+                itemPrice[i] = 0;
+                document.getElementById("item"+i).checked = false;
+                addTotalItem();
+            }
+            item[i] = "deleted";
+        }
+        
+        function addItem(i,quantity){          
+            quantity = parseInt(quantity);
+            itemQuantity[i] = quantity;
+            itemPrice[i] = parseFloat(orderData[product[i]]["unitPrice"]) * quantity;
+            document.getElementById("totalItemPrice"+i).innerHTML = itemPrice[i];
+            
+            if (document.getElementById("item"+i).checked){
+                item[i] = "selected";
+                addTotalItem();
+            }
+            else{
+                if (item[i] == "selected"){
+                    itemQuantity[i] = 0;
+                    itemPrice[i] = 0;
+                    addTotalItem();
+                    document.getElementById("selectAll").checked = false;
+                    item[i] = "unselected";
+                }
+            }
+        }
+
+        function addTotalItem(){
+            var totalPrice = 0, totalQuantity = 0;
+            for (var i = 0; i < product.length; i++){
+                itemQuantity[i] = parseInt(itemQuantity[i]);
+                itemPrice[i] = parseInt(itemPrice[i]);
+                totalQuantity += itemQuantity[i];
+                totalPrice += itemPrice[i];
+            }
+            document.getElementById("totalItem").innerHTML = totalQuantity;
+            document.getElementById("totalPrice").innerHTML = totalPrice;
+        }
+        
+        function checkout(){
+            const selectedProduct = [], selectedQty = [];
+            for (var i = 0; i < product.length; i++){
+                if (item[i] == "selected"){
+                    selectedProduct.push(product[i]);
+                    localStorage.setObj("selectedProduct", selectedProduct);
+                    selectedQty.push(qty[i]);
+                    localStorage.setObj("selectedQty", selectedQty);
+                }
+            }
+        }
+    });
+
+
+
+
 
