21e161ce981fbbd792eeb91bc20156be63b2ead9
LWMeng0822 <88777384+LWMeng0822@users.noreply.github.com>
2021-09-13 21:10:19 +0800

    Add files via upload

--- GTree-furniture-website/Register/register.js
+++ GTree-furniture-website/Register/register.js
@@ -14,28 +14,31 @@ function myfun(){
 
   if(a == ""){
     document.getElementById("messages").innerHTML="**Please Fill Your Password";
+    alert("Please Fill Your Password");
     return false;
   }
   
   if(a.length > 25){
     document.getElementById("messages").innerHTML="**Password length must be smaller than 25 characters";
+    alert("Password length must be smaller than 25 characters");
     return false;
   }
   
   if(a.length < 5){
     document.getElementById("messages").innerHTML="**Password length must be greater than 5 characters";
+    alert("Password length must be greater than 5 characters");
     return false;
   }
 
   if(a != b){
-    alert("Your Password is not same");
     setTimeout(function(){ location.href = "../Register/register.html"; }, 0.000001);
+    alert("Your Password is not same");
     return false;
   }
 
   else{
-    alert("Register Successfully");
     setTimeout(function(){ location.href = "../Login/login.html"; }, 0.000001);
+    alert("Register Successfully");
     return true;
   }
 }
