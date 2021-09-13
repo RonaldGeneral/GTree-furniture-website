6dcbd35c111ff2731e9a877382526bfe6340a98f
weixiang1120 <81514513+weixiang1120@users.noreply.github.com>
2021-09-13 23:08:07 +0800

    Add files via upload

--- GTree-furniture-website/Personal-particulars/personal-particulars.js
+++ GTree-furniture-website/Personal-particulars/personal-particulars.js
@@ -1,9 +1,26 @@
+Storage.prototype.setObj = function(key, obj) {
+	return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getObj = function(key) {
+	  return JSON.parse(this.getItem(key))
+}
+
+document.getElementById("name").value = localStorage.getObj("userName");
+document.getElementById("gender").value = localStorage.getObj("userGender");
+document.getElementById("eadres").value = localStorage.getObj("userBirthday");
+document.getElementById("mnum").value = localStorage.getObj("userPhoneNo");
+document.getElementById("adres").value = localStorage.getObj("userAddress");
+document.getElementById("email").value = localStorage.getObj("userEmail");
+document.getElementById("password").value = localStorage.getObj("userPassword");
+
 function disabledName() {
 	var x = document.getElementById("name");
 	if(x.hasAttribute("disabled")){
 		x.removeAttribute("disabled");
-    } else {
-    	x.setAttribute("disabled",""); 
+    }
+	else {
+    	x.setAttribute("disabled","");
+        localStorage.setObj("userName", x.value);
     }
 }
 
@@ -11,16 +28,20 @@ function disabledGender() {
 	var x = document.getElementById("gender");
 	if(x.hasAttribute("disabled")){
 		x.removeAttribute("disabled");
-    } else {
+    }
+	else {
     	x.setAttribute("disabled",""); 
+		localStorage.setObj("userGender", x.value);
     }
 }
 function disabledEadres() {
 	var x = document.getElementById("eadres");
 	if(x.hasAttribute("disabled")){
 		x.removeAttribute("disabled");
-    } else {
-    	x.setAttribute("disabled",""); 
+    }
+	else {
+    	x.setAttribute("disabled","");
+		localStorage.setObj("userBirthday", x.value);
     }
 }
 function disabledMnum() {
@@ -28,23 +49,38 @@ function disabledMnum() {
 	if(x.hasAttribute("disabled")){
 		x.removeAttribute("disabled");
     } else {
-    	x.setAttribute("disabled",""); 
+    	x.setAttribute("disabled","");
+		localStorage.setObj("userPhoneNo", x.value);
     }
 }
+
 function disabledEmail() {
 	var x = document.getElementById("email");
 	if(x.hasAttribute("disabled")){
 		x.removeAttribute("disabled");
     } else {
-    	x.setAttribute("disabled",""); 
+    	x.setAttribute("disabled","");
+		localStorage.setObj("userEmail", x.value); 
     }
 }
+
 function disabledAdres() {
 	var x = document.getElementById("adres");
 	if(x.hasAttribute("disabled")){
 		x.removeAttribute("disabled");
     } else {
+    	x.setAttribute("disabled","");
+		localStorage.setObj("userAddress", x.value);
+    }
+}
+
+function disabledPassword() {
+	var x = document.getElementById("password");
+	if(x.hasAttribute("disabled")){
+		x.removeAttribute("disabled");
+    } else {
     	x.setAttribute("disabled",""); 
+		localStorage.setObj("userPassword", x.value);
     }
 }
 
