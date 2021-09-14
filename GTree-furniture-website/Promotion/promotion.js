4631825cbd114baad9d9c750e5583c1765ffb808
LWMeng0822 <88777384+LWMeng0822@users.noreply.github.com>
2021-09-14 17:44:00 +0800

    Add files via upload

--- GTree-furniture-website/Promotion/promotion.js
+++ GTree-furniture-website/Promotion/promotion.js
@@ -1,28 +1,31 @@
+//declare variable for function below
 var i = 0;
 var images = [];
 var links = [];
 var time = 3000;
 
+//change images via array
 images[0] = 'promotion-assets/bed(1).jpg';
 images[1] = 'promotion-assets/dinningtable(1).jpg';
 images[2] = 'promotion-assets/sofa(1).jpg';
 images[3] = 'promotion-assets/shelvesdecoration(5).jpg';
 
+//change links via array
 links[0] = '../productcatalog/bedroom/bed/detailsbed1.html';
 links[1] = '../productcatalog/table/detailsdining-table1.html';
 links[2] = '../productcatalog/chair/detailssofa1.html';
 links[3] = '../productcatalog/shelves/detailswardrobe1.html';
 
-//change slide
 function changeSlides(){
+    //change images via array
     var slide = document.getElementById("slide");
     slide.src = images[i];
 
-    //change links
+    //change links via array
     var linkBox = document.getElementById("linkBox");
     linkBox.href = links[i];
     
-    //fill black dot
+    //change active dot
     var dot = document.getElementsByClassName('dot');
     if(i > 0){
         dot[i-1].classList.remove("active");
@@ -42,15 +45,6 @@ function changeSlides(){
     //looping the func wif delay
     setTimeout("changeSlides()", time);
 }
+//auto load when access the webpage
 window.onload = changeSlides();
-
-/*var zuo=document.getElementById("zuo");
-var n=1;
-
-setInterval(function(){
-    if(n>4) n=1
-    zuo.innerHTML="<img src='promotion-assets/product("+n+").png'/>";
-    n++;
-
-},2000);*/
 
